e1dab46d418dc700213d37a296a6c509872578d8
SWD-002\falma <falmars.g@gmail.com>
2022-03-08 04:25:05 +0900

    音を出す機能を追加

--- Assets/Keys/Scripts/MakeaSound.cs
+++ Assets/Keys/Scripts/MakeaSound.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MakeaSound: MonoBehaviour
 {
@@ -25,15 +26,96 @@ public class MakeaSound: MonoBehaviour
         E6
     }
     [SerializeField] private Key key;
-    // Start is called before the first frame update
+    AudioSource ad;
+    AudioClip ac;
+
     void Start()
     {
-        
+        ad = GetComponent<AudioSource>();
+        ac = ad.clip;
+        ad.pitch = Mathf.Pow(2.0f, (float)(key)/12.0f);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        switch (key)
+        {
+            case Key.C5:
+                KeyPush(KeyCode.A);
+                break;
+            case Key.Cis5:
+                KeyPush(KeyCode.W);
+                break;
+            case Key.D5:
+                KeyPush(KeyCode.S);
+                break;
+            case Key.Dis5:
+                KeyPush(KeyCode.E);
+                break;
+            case Key.E5:
+                KeyPush(KeyCode.D);
+                break;
+            case Key.F5:
+                KeyPush(KeyCode.F);
+                break;
+            case Key.Fis5:
+                KeyPush(KeyCode.T);
+                break;
+            case Key.G5:
+                KeyPush(KeyCode.G);
+                break;
+            case Key.Gis5:
+                KeyPush(KeyCode.Y);
+                break;
+            case Key.A5:
+                KeyPush(KeyCode.H);
+                break;
+            case Key.Ais5:
+                KeyPush(KeyCode.U);
+                break;
+            case Key.H5:
+                KeyPush(KeyCode.J);
+                break;
+            case Key.C6:
+                KeyPush(KeyCode.K);
+                break;
+            case Key.Cis6:
+                KeyPush(KeyCode.O);
+                break;
+            case Key.D6:
+                KeyPush(KeyCode.L);
+                break;
+            case Key.Dis6:
+                KeyPush(KeyCode.P);
+                break;
+            case Key.E6:
+                KeyPush(KeyCode.Colon);
+                break;
+            default:
+                break;
+        }    
+    }
+
+    void KeyPush(KeyCode code)
+    {
+        if (Input.GetKeyDown(code))
+        {
+            Sound();
+        }
+        else if (Input.GetKeyUp(code))
+        {
+            ad.Stop();
+        }
+    }
+
+    public void Sound()
+    {
+        ad.PlayOneShot(ac);
+    }
+
+
+    public void OnClickUp()
+    {
+        ad.Stop();
     }
 }
