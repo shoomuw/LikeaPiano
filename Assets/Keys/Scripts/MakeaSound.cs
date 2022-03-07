25b2da62df2ffaab05bd9886f35d1a7de054adb1
SWD-002\falma <falmars.g@gmail.com>
2022-03-07 23:37:06 +0900

    鍵盤追加

---
+++ Assets/Keys/Scripts/MakeaSound.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeaSound: MonoBehaviour
+{
+    enum Key
+    {
+        C5, 
+        Cis5,
+        D5,
+        Dis5,
+        E5,
+        F5,
+        Fis5,
+        G5,
+        Gis5,
+        A5,
+        Ais5,
+        H5,
+        C6,
+        Cis6,
+        D6,
+        Dis6,
+        E6
+    }
+    [SerializeField] private Key key;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
